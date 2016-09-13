e990165c6abd736f59f4c411874c87775b2b1787
Rosie Allott <rosie.allott@gmail.com>
2016-09-13 16:43:23 +0100

    Thermostat reset button and energy colour

--- jasmine/spec/ThermostatSpec.js
+++ jasmine/spec/ThermostatSpec.js
@@ -14,6 +14,25 @@ describe ("Thermostat", function() {
     });
   });
 
+  describe ('#switchPowerMode', function(){
+    it('should switch power mode between true and false', function(){
+      thermostat.switchPowerMode();
+      expect(thermostat.powersave).toEqual (false);
+    });
+    it('should switch power mode between true and false', function(){
+      thermostat.switchPowerMode();
+      thermostat.switchPowerMode();
+      expect(thermostat.powersave).toEqual (true);
+    });
+  });
+
+  describe ('#reset button', function(){
+    it('should reset temperature to 20C', function(){
+      thermostat.resetTemp();
+      expect(thermostat.temperature).toEqual(20);
+    });
+  });
+
   describe ('temperature management', function(){
     it('increases temperature with up button', function() {
       thermostat.increase();
@@ -35,15 +54,30 @@ describe ("Thermostat", function() {
 
   describe ('edge cases', function(){
     it('throws error if decreased below 10C', function(){
-      expect(function() {thermostat.decrease(11)}).toThrowError('cannot go below 10C')
+      expect(function() {thermostat.decrease(11)}).toThrowError('cannot go below 10C');
     });
 
     it('throws error if powersave mode is on and temperature is above 25', function(){
-      expect(function() {thermostat.increase(6)}).toThrowError('cannot go above 25C')
-    })
+      expect(function() {thermostat.increase(6)}).toThrowError('cannot go above 25C');
+    });
     it('throws error if powersave mode is off and temperature is above 32', function(){
-      thermostat.powersave = false
-      expect(function() {thermostat.increase(13)}).toThrowError('cannot go above 32C')
-    })
+      thermostat.powersave = false;
+      expect(function() {thermostat.increase(13)}).toThrowError('cannot go above 32C');
+    });
+  });
+
+  describe('energy descriptors', function(){
+    it('should show low energy if below 18C', function(){
+      thermostat.decrease(3);
+      expect(thermostat.displayEnergyUsage()).toEqual('low-energy');
+    });
+    it('should show medium energy if between 18C and 25C', function(){
+      expect(thermostat.displayEnergyUsage()).toEqual('medium-energy');
+    });
+    it('should show high energy if over 25C', function(){
+      thermostat.switchPowerMode();
+      thermostat.increase(6);
+      expect(thermostat.displayEnergyUsage()).toEqual('high-energy');
+    });
   });
 });
--- jasmine/src/Thermostat.js
+++ jasmine/src/Thermostat.js
@@ -3,7 +3,6 @@
 function Thermostat() {
   this.temperature = 20;
   this.powersave = true;
-
   this.MAX = {true: 25, false: 32};
 }
 
@@ -23,5 +22,22 @@ Thermostat.prototype = {
     else {
       this.temperature -= amount;
     }
+  },
+  switchPowerMode: function(){
+    this.powersave = !this.powersave
+  },
+  resetTemp: function(){
+    this.temperature = 20
+  },
+  displayEnergyUsage: function() {
+    if(this.temperature < (18)) {
+      return 'low-energy'
+    }
+    else if(this.temperature < (25)) {
+      return 'medium-energy'
+    }
+    else {
+      return 'high-energy'
+    }
   }
-}
+};
