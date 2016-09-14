e46e1e10c658a164226295090080161e42b4e863
ercekal <ercekalabalikoglu@gmail.com>
2016-09-14 16:20:39 +0100

    jQuery part just started

--- src/thermostat.js
+++ src/thermostat.js
@@ -2,56 +2,54 @@
 DEFAULT_TEMPERATURE = 20;
 
 var Thermostat = function Thermostat(){"use strict";
-  this._temperature = DEFAULT_TEMPERATURE;
-  this._minTemperature = 10;
-  this._powerSaving = true;
-  this._powerSaveMax = 25;
-  this._maxTemperature = 32;
+this._temperature = DEFAULT_TEMPERATURE;
+this._minTemperature = 10;
+this._powerSaving = true;
+this._powerSaveMax = 25;
+this._maxTemperature = 32;
 };
 
 Thermostat.prototype ={
   showTemperature: function () {
-
-  window.getElementById("demo").innerHTML = showTemperature();
-  return this._temperature;
-},
+    return this._temperature;
+  },
   increaseTemperature: function () {
-      if(this._temperature >= this.currentPower()){
-        throw new Error("temperature cannot go above max limit.");
-      } else {
-        this._temperature += 1;
-}},
-  decreaseTemperature: function () {
-    if (this._temperature <= this._minTemperature){
-      throw new Error("temperature cannot go below minimum.");
+    if(this._temperature >= this.currentPower()){
+      throw new Error("temperature cannot go above max limit.");
     } else {
-      this._temperature -= 1;
-}},
-  resetTemperature: function() {
-    return this._temperature = DEFAULT_TEMPERATURE;
-  },
+      return this._temperature += 1;
 
-  currentPower : function() {
-  if(this._powerSaving === true){
-    return this._powerSaveMax;
-  } else {
-    return this._maxTemperature;
-  }},
-
-  switchPowerSaving: function(){
-      if(this._powerSaving === true){
-        return this._powerSaving === false;
+    }},
+    decreaseTemperature: function () {
+      if (this._temperature <= this._minTemperature){
+        throw new Error("temperature cannot go below minimum.");
       } else {
-        return this._powerSaving === true;
-      }
-  },
+        return this._temperature -= 1;
+      }},
+      resetTemperature: function() {
+        return this._temperature = DEFAULT_TEMPERATURE;
+      },
 
-  displayEfficiency : function() {
-  if(this._temperature < 18) {
-    return "Green";
-  }else if (this._temperature < 25) {
-    return "Yellow";
-  }else {
-    return "Red";
-  }}
-};
+      currentPower : function() {
+        if(this._powerSaving === true){
+          return this._powerSaveMax;
+        } else {
+          return this._maxTemperature;
+        }},
+
+        switchPowerSaving: function(){
+          if (this._powerSaving === true) {
+            this._powerSaving = false;
+          } else {
+            this._powerSaving = true;
+          }},
+
+          displayEfficiency : function() {
+            if(this._temperature < 18) {
+              return "Green";
+            }else if (this._temperature < 25) {
+              return "Yellow";
+            }else {
+              return "Red";
+            }}
+          };
