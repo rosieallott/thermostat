1a19b415c34f19fa7b4303a5247a645fee25a66a
ercekal <ercekalabalikoglu@gmail.com>
2016-09-15 11:56:17 +0100

    city ID was added staticly

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -1,58 +1,58 @@
 
 describe("feature test", function(){ "use strict";
 
-  var thermostat;
+var thermostat;
 
-  beforeEach(function(){
-    thermostat = new Thermostat();
-  });
+beforeEach(function(){
+  thermostat = new Thermostat();
+});
 
 
-  it("Has an initial temperature of 20", function(){
-    expect(thermostat.showTemperature()).toEqual(20);
-  });
+it("Has an initial temperature of 20", function(){
+  expect(thermostat.showTemperature()).toEqual(20);
+});
 
-  it("can be increased in temperature", function(){
-    thermostat.increaseTemperature();
-    expect(thermostat.showTemperature()).toEqual(21);
-  });
+it("can be increased in temperature", function(){
+  thermostat.increaseTemperature();
+  expect(thermostat.showTemperature()).toEqual(21);
+});
 
-  it("can be decreased in temperature", function(){
-    thermostat.decreaseTemperature();
-    expect(thermostat.showTemperature()).toEqual(19);
-  });
+it("can be decreased in temperature", function(){
+  thermostat.decreaseTemperature();
+  expect(thermostat.showTemperature()).toEqual(19);
+});
 
-  it("cannot be decreased in temperature past minimum", function(){
-    thermostat._temperature = 10;
-    expect(function(){thermostat.decreaseTemperature();}).toThrowError("temperature cannot go below minimum.");
-  });
+it("cannot be decreased in temperature past minimum", function(){
+  thermostat._temperature = 10;
+  expect(function(){thermostat.decreaseTemperature();}).toThrowError("temperature cannot go below minimum.");
+});
 
-  it("cannot be increased in temperature past powersave(turned on) max", function(){
-    thermostat._temperature = 25;
-    expect(function(){thermostat.increaseTemperature();}).toThrowError("temperature cannot go above max limit.");
-  });
+it("cannot be increased in temperature past powersave(turned on) max", function(){
+  thermostat._temperature = 25;
+  expect(function(){thermostat.increaseTemperature();}).toThrowError("temperature cannot go above max limit.");
+});
 
-  it("cannot be increased in temperature past powersave(turned off) max", function(){
-    thermostat._temperature = 32;
-    expect(function(){thermostat.increaseTemperature();}).toThrowError("temperature cannot go above max limit.");
-  });
+it("cannot be increased in temperature past powersave(turned off) max", function(){
+  thermostat._temperature = 32;
+  expect(function(){thermostat.increaseTemperature();}).toThrowError("temperature cannot go above max limit.");
+});
 
-  it("can change temperature back to default on reset", function(){
-    thermostat._temperature = 32;
-    thermostat.resetTemperature();
-    expect(thermostat.showTemperature()).toEqual(20);
-  });
+it("can change temperature back to default on reset", function(){
+  thermostat._temperature = 32;
+  thermostat.resetTemperature();
+  expect(thermostat.showTemperature()).toEqual(20);
+});
 
-  it('should display green when temperature is below 18 degrees', function() {
-    thermostat._temperature = 17;
-    expect(thermostat.displayEfficiency()).toEqual("Green");
-  });
-  it('should display yellow when temperature is between 18 & 25 degrees', function() {
-    thermostat._temperature = 23;
-    expect(thermostat.displayEfficiency()).toEqual("Yellow");
-  });
-  it('should display red when temperature is above 25 degrees', function() {
-    thermostat._temperature = 28;
-    expect(thermostat.displayEfficiency()).toEqual("Red");
-  });
+it('should display green when temperature is below 18 degrees', function() {
+  thermostat._temperature = 17;
+  expect(thermostat.displayEfficiency()).toEqual("Green");
+});
+it('should display yellow when temperature is between 18 & 25 degrees', function() {
+  thermostat._temperature = 23;
+  expect(thermostat.displayEfficiency()).toEqual("Yellow");
+});
+it('should display red when temperature is above 25 degrees', function() {
+  thermostat._temperature = 28;
+  expect(thermostat.displayEfficiency()).toEqual("Red");
+});
 });
--- src/interface.js
+++ src/interface.js
@@ -4,12 +4,19 @@ $(document).ready(function() {
   updateTemperature();
   $('#powersavingstatus').text(thermostat._powerSaving);
 
+  $.get("http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=b15f5f9da7b49eb2b3f5c00b679f6db9", function(data) {
+    $("#current").text(Math.round(data.main.temp - 273));
+
+    // console.log(data.main.temp - 273);
+  });
+
+
   $('#increase').click(function() {
     thermostat.increaseTemperature();
     updateTemperature();
   });
 
-  $('#decrease').click(function() {
+  $('#decrease').click(function() { getElementById('#decrease')
     thermostat.decreaseTemperature();
     updateTemperature();
   });
@@ -28,4 +35,10 @@ $(document).ready(function() {
     $('#temperature').text(thermostat._temperature);
     $('#temperature').attr('class', thermostat.displayEfficiency());
   }
+
 });
+
+
+
+
+// {"coord":{"lon":-0.13,"lat":51.51},"weather":[{"id":721,"main":"Haze","description":"haze","icon":"50d"},{"id":701,"main":"Mist","description":"mist","icon":"50d"},{"id":741,"main":"Fog","description":"fog","icon":"50d"}],"base":"stations","main":{"temp":295.8,"pressure":1009,"humidity":64,"temp_min":290.15,"temp_max":301.55},"visibility":10000,"wind":{"speed":4.1,"deg":40},"clouds":{"all":0},"dt":1473933951,"sys":{"type":1,"id":5091,"message":0.0443,"country":"GB","sunrise":1473917797,"sunset":1473963193},"id":2643743,"name":"London","cod":200}
